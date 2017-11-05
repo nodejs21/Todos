f264c4e6139fe3c4b48f75b8a69bf3e6e520ce37
root <nodejs21@gmail.com>
2017-11-05 20:08:43 +0500

    email and password added in body, returns the whole object of the logged in person

--- routes/signIn.js
+++ routes/signIn.js
@@ -8,15 +8,15 @@ var router = express.Router();
 var {mongoose} = require('./../db/mongoose');
 var {people} = require('./../models/people');
 
-router.post('/:email/:password', (req, res, next) => {
-    var user_email = req.params.email;
-    var user_password = req.params.password;
+router.post('/', (req, res, next) => {
+    var user_email = req.body.email;
+    var user_password = req.body.password;
     people.find({email: user_email}).then((user) => {
         if(user.length == 0) {
             return res.send(`no user against: ${user_email}`);
         }
         if(user[0].password === user_password) {
-            return res.send("Welcome!!");
+            return res.json(user);
         }
         user = undefined;
         return res.send("access denied!!");
